85c6df9bac70011ec935c6f2df4e6493e6e42359
marcosR-silva <marcos.1998226@gmail.com>
2021-06-29 22:21:47 -0300

    Refactoring the test cause it was saving data into the dataBase in test sessions

--- src/tests/models/categoryModel.test.js
+++ src/tests/models/categoryModel.test.js
@@ -1,6 +1,16 @@
 const { expect } = require('chai');
 
-const { Categories } = require('../../models')
+const SequelizeMock = require('sequelize-mock');
+const DBConnectionMock = new SequelizeMock();
+
+
+const CategoryMock = DBConnectionMock.define('Categories', {}, {
+  instanceMethods: {
+      myTestFunc: function () {
+          return 'Test Categories';
+      },
+  },
+});
 
 describe('Inserting a new category on database', () => {
 
@@ -11,13 +21,25 @@ describe('Inserting a new category on database', () => {
   describe('When inserted with success', () => {
 
     it(' should return an object', async () => {
-      const response = await Categories.create(payloadCategory);
+      const response = await CategoryMock.create(payloadCategory);
+      console.log('my test', response);
       expect(response).to.be.a('object');
     });
 
     it('The objeto has the category "id" ', async () => {
-      const response = await Categories.create(payloadCategory);
+      const response = await CategoryMock.create(payloadCategory);
       expect(response).to.have.a.property('id');
+    });
+
+    it('The object has the attribute "name" ', async () => {
+      const response = await CategoryMock.create(payloadCategory);
+      expect(response).to.have.a.property('name');
+    });
+
+    it('The object has the attribute "xablau" ', async () => {
+      const response = await CategoryMock.create(payloadCategory);
+      expect(response).to.have.a.property('xablau');
+      //should fail
     });
   });
 });
--- src/tests/models/deviceModel.test.js
+++ src/tests/models/deviceModel.test.js
@@ -1,26 +1,36 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-const { Devices } = require('../../models')
+const SequelizeMock = require("sequelize-mock");
+const DBConnectionMock = new SequelizeMock();
 
-describe('Inserting a new device on the database', () => {
+const DeviceMock = DBConnectionMock.define(
+  "Devices",{},
+  {
+    instanceMethods: {
+      myTestFunc: function () {
+        return "Test Devices";
+      },
+    },
+  }
+);
 
+describe("Inserting a new device on the database", () => {
   const payloadCategory = {
-    "category": "sasasa",
-    "color": "111111",
-    "partNumber": 78787,
-    "categoryId": 1
- }
+    category: "sasasa",
+    color: "111111",
+    partNumber: 78787,
+    categoryId: 1,
+  };
 
-  describe('When inserted with success', () => {
-
-    it(' should return an object', async () => {
-      const response = await Devices.create(payloadCategory);
-      expect(response).to.be.a('object');
+  describe("When inserted with success", () => {
+    it(" should return an object", async () => {
+      const response = await DeviceMock.create(payloadCategory);
+      expect(response).to.be.a("object");
     });
 
     it('The objeto has the device "id" ', async () => {
-      const response = await Devices.create(payloadCategory);
-      expect(response).to.have.a.property('id');
+      const response = await DeviceMock.create(payloadCategory);
+      expect(response).to.have.a.property("id");
     });
   });
 });
